4f9de757dfbbf5428116144856cb1f210f994847
Dmitry Medvedev <medvedev@sbigroup.vc>
2016-01-16 13:12:10 +0600

    Added tapper app files

--- tapper-app/ViewController.swift
+++ tapper-app/ViewController.swift
@@ -8,18 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var tapsText: UITextField!
+    
+    @IBOutlet weak var starButton: UIButton!
+    @IBOutlet weak var tapsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tapsText.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func onPlayButtonClick (sender: UIButton){
+        
+        if tapsText.text != nil && tapsText.text != ""{
+            logoImage.hidden = true
+            playButton.hidden = true
+            tapsText.hidden = true
+            
+            starButton.hidden = false
+            tapsLabel.hidden = false
+            
+            maxTaps = Int(tapsText.text!)!
+            currentTaps = 0
+            updateTaps()
+        }
     }
-
+    
+    @IBAction func onStarButton (sender: UIButton){
+        
+        currentTaps++;
+        updateTaps()
+        
+        if currentTaps >= maxTaps{
+            maxTaps = 0
+            logoImage.hidden = false
+            playButton.hidden = false
+            tapsText.hidden = false
+            
+            starButton.hidden = true
+            tapsLabel.hidden = true
+            
+            tapsText.text = ""
+        }
+    }
+    
+    func updateTaps (){
+        tapsLabel.text = "\(currentTaps) Taps"
+    }
+    
+    func textFieldShouldReturn(userText: UITextField) -> Bool {
+        userText.resignFirstResponder()
+        return true;
+    }
+    
 
 }
-
